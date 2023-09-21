37507689d89adbe86c3eadb3f449981c0055f115
zhongkai <1980274824@qq.com>
2023-09-21 18:01:47 +0800

    自动装配

--- phoenix-loadbalance/src/main/java/com/kuafoo4j/configuration/LoadBalanceAutoConfiguration.java
+++ phoenix-loadbalance/src/main/java/com/kuafoo4j/configuration/LoadBalanceAutoConfiguration.java
@@ -4,9 +4,18 @@ import com.kuafoo4j.core.client.*;
 import com.kuafoo4j.loadbalance.IPhoenixServerDiscovery;
 import com.kuafoo4j.loadbalance.PhoenixRoundLoadBalance;
 import com.kuafoo4j.loadbalance.ServerDiscoveryAdaptor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 自动装配
@@ -14,6 +23,30 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class LoadBalanceAutoConfiguration {
+
+    @Autowired
+    private List<RestTemplate> restTemplateList = new ArrayList<>();
+
+    /**
+     * SmartInitializingSingleton: Springboot扩展点,当所有单例 bean 都初始化完成以后，
+     * Spring的IOC容器会回调该接口的 afterSingletonsInstantiated()方法。
+     *
+     * 使用给RestTemplate添加loadBalance拦截器
+     * @param loadBalanceInterceptor
+     * @return
+     */
+    @Bean
+    public SmartInitializingSingleton loadBalancedRestTemplateInitializer(LoadBalanceInterceptor loadBalanceInterceptor) {
+        return () -> {
+            for (RestTemplate restTemplate : this.restTemplateList) {
+                List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+                // 添加拦截器
+                interceptors.add(loadBalanceInterceptor);
+                restTemplate.setInterceptors(interceptors);
+            }
+        };
+    }
+
 
     /**
      * 轮询算法-轮询算法
